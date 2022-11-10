8864a2871efc3808a18274688fae3b46c0bfab28
danendraraharjo <danendraraharjo@gmail.com>
2022-11-10 18:54:17 +0700

    lama

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
+#byog-python-flask/app.py
 from flask import Flask, request, render_template, redirect
-
 app = Flask(__name__)
-
 @app.get("/")
-def index():
-    return render_template( "index.html" )
-
+def index ():
+    """
+    TODO: Render the home page provided under templates/index.html in the repository
+    """
+    return "TODO"
 @app.get("/search")
-def search():
-    args = request.args.get( "q" )
-    return redirect( f"https://google.com/search?q= {args} " )
-
-@app.get("/lucky")
-def lucky():
-    args = request.args.get( "q" )
-    return redirect( f"https://www.google.com/search?q={args}&btnI=&sourceid=navclient&gfns=1" )
-
-if __name__ == "__main__":
+def search ():
+    """
+    TODO:
+    1. Capture the word that is being searched
+    2. Search for the word on Google and display results
+    """
+    return "TODO"
+if __name__ == "__main__" :
     app.run()
