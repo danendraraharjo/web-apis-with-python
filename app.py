43a28583ea1159fa2ce4abd82ee3e2ba95c55062
danendraraharjo <danendraraharjo@gmail.com>
2022-11-10 18:50:06 +0700

    baru

--- app.py
+++ app.py
@@ -4,19 +4,17 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    """
-    TODO: Render the home page provided under templates/index.html in the repository
-    """
-    return "TODO"
+    return render_template( "index.html" )
 
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    args = request.args.get( "q" )
+    return redirect( f"https://google.com/search?q= {args} " )
+
+@app.get("/lucky")
+def lucky():
+    args = request.args.get( "q" )
+    return redirect( f"https://www.google.com/search?q={args}&btnI=&sourceid=navclient&gfns=1" )
 
 if __name__ == "__main__":
     app.run()
